1652238bf3408209e576e05708623afbcc6ec364
Ravi.Cherukuri <vcheruk2@ncsu.edu>
2020-03-18 18:37:10 -0700

    Initial commit.

---
+++ src/test/java/com/ravi/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
